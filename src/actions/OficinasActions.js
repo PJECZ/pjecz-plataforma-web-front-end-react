8efbbe7d36a6c298291c00954546f395ec1928a6
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-05-26 15:22:35 -0500

    Mejora en OficinasActions

--- src/actions/OficinasActions.js
+++ src/actions/OficinasActions.js
@@ -10,24 +10,10 @@ export const Oficinas = (filtros) => {
             if (filtros.distrito_id) {
                 ruta = ruta + `?distrito_id=${filtros.distrito_id}`
             }
+            if (filtros.domicilio_id) {
+                ruta = ruta + `?domicilio_id=${filtros.domicilio_id}`
+            }
             HttpCliente.get(ruta, access_token)
-                .then(response => {
-                    resolve(response)
-                })
-                .catch(error => {
-                    resolve(error.response)
-                })
-        }
-    })
-}
-
-
-export const OficinasDelDistrito = (distrito_id) => {
-    return new Promise((resolve, reject) => {
-        const data = JSON.parse(window.localStorage.getItem('data'))
-        if (data) {
-            const { access_token } = data
-            HttpCliente.get(`/v1/oficinas?distrito_id=${distrito_id}`, access_token)
                 .then(response => {
                     resolve(response)
                 })
--- src/components/distritos/DetailDistrito.js
+++ src/components/distritos/DetailDistrito.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
 import { Card, CardContent, Grid, TextField, Typography } from '@mui/material'
 
 import commonSX from '../../theme/CommonSX'
