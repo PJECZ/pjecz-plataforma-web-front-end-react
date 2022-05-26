6b89d3cff7d2d649465ca2653dd4bd50e4e90eaa
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-05-26 16:07:15 -0500

    En el detalle de los domicilios se ven las oficinas

--- src/components/domicilios/ListDomiciliosScreen.js
+++ src/components/domicilios/ListDomiciliosScreen.js
@@ -1,68 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { Card, Container, Paper, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from '@mui/material'
+import React from 'react'
 
-import commonSX from '../../theme/CommonSX'
-import Progress from '../ui/Progress'
-
-import { Domicilios } from '../../actions/DomiciliosActions'
+import CommonScreen from '../ui/CommonScreen'
+import ListDomicilios from './ListDomicilios'
 
 
 const ListDomiciliosScreen = () => {
 
-    // Redirigir al login cuando no haya iniciado sesion
-    const data = JSON.parse(window.localStorage.getItem('data'))
-    const navigate = useNavigate()
-    useEffect(() => {
-        if (!data) {
-            navigate('/login')
-        }
-    })
-
-    // Consultar Domicilios
-    const [datos, setDatos] = useState([])
-    const [consultado, setConsultado] = useState(false)
-    useEffect(() => {
-        async function fetchData() {
-            const response = await Domicilios()
-            if (response.status === 200) {
-                setDatos(response.data.items)
-                setConsultado(true)
-            }
-        }
-        fetchData()
-    }, [])
-
-    if (consultado) {
-        return (
-            <Container sx={commonSX.container}>
-                <Typography variant='h5' sx={commonSX.title}>
-                    Domicilios
-                </Typography>
-                <Card>
-                    <TableContainer componet={Paper}>
-                        <Table size="small">
-                            <TableBody>
-                                {datos.map((item, indice) =>
-                                    <TableRow key={indice}>
-                                        <TableCell>
-                                            {item.completo}
-                                        </TableCell>
-                                    </TableRow>
-                                )}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                </Card>
-            </Container>
-        )
-    } else {
-        return (
-            <Container sx={commonSX.container}>
-                <Progress />
-            </Container>
-        )
-    }
+    return (
+        <CommonScreen>
+            <ListDomicilios />
+        </CommonScreen>
+    )
 
 }
 
