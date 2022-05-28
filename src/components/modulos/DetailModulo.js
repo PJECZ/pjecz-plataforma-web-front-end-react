35c0743689e5c8f1e707b3b0ea425c8d3a573508
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-05-28 07:26:43 -0500

    Avance en Modulos

---
+++ src/components/modulos/DetailModulo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const DetailModulo = () => {
+
+    return (
+        <div>DetailModulo</div>
+    )
+
+}
+
+export default DetailModulo
--- src/components/modulos/ListModulosScreen.js
+++ src/components/modulos/ListModulosScreen.js
@@ -1,68 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { Card, Container, Paper, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from '@mui/material'
+import React from 'react'
 
-import commonSX from '../../theme/CommonSX'
-import Progress from '../ui/Progress'
-
-import { Modulos } from '../../actions/ModulosActions'
+import CommonScreen from '../ui/CommonScreen'
+import ListModulos from 'ListModulos'
 
 
 const ListModulosScreen = () => {
 
-    // Redirigir al login cuando no haya iniciado sesion
-    const data = JSON.parse(window.localStorage.getItem('data'))
-    const navigate = useNavigate()
-    useEffect(() => {
-        if (!data) {
-            navigate('/login')
-        }
-    })
-
-    // Consultar Modulos
-    const [datos, setDatos] = useState([])
-    const [consultado, setConsultado] = useState(false)
-    useEffect(() => {
-        async function fetchData() {
-            const response = await Modulos()
-            if (response.status === 200) {
-                setDatos(response.data.items)
-                setConsultado(true)
-            }
-        }
-        fetchData()
-    }, [])
-
-    if (consultado) {
-        return (
-            <Container sx={commonSX.container}>
-                <Typography variant='h5' sx={commonSX.title}>
-                    Modulos
-                </Typography>
-                <Card>
-                    <TableContainer componet={Paper}>
-                        <Table size="small">
-                            <TableBody>
-                                {datos.map((item, indice) =>
-                                    <TableRow key={indice}>
-                                        <TableCell>
-                                            {item.nombre}
-                                        </TableCell>
-                                    </TableRow>
-                                )}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                </Card>
-            </Container>
-        )
-    } else {
-        return (
-            <Container sx={commonSX.container}>
-                <Progress />
-            </Container>
-        )
-    }
+    return (
+        <CommonScreen>
+            <ListModulos />
+        </CommonScreen>
+    )
 
 }
 
