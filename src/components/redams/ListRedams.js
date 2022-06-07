5ee4b34d8ccd95a7ff1194532612f5d63e69880f
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-06-07 14:30:38 -0500

    Avance

--- src/components/redams/ListRedams.js
+++ src/components/redams/ListRedams.js
@@ -1,1 +1,28 @@
 import React from 'react'
+
+import { Readms } from '../../actions/RedamsActions'
+
+
+const ListRedams = () => {
+
+    // Consultar REDAM
+    const [redams, setRedams] = useState([])
+    const [consultado, setConsultado] = useState(false)
+    useEffect(() => {
+        async function fetchData() {
+            const response = await Redams(props)
+            if (response.status === 200) {
+                setRedams(response.data.items)
+                setConsultado(true)
+            }
+        }
+        fetchData()
+    }, [props])
+
+    return (
+        <div>ListRedams</div>
+    )
+
+}
+
+export default ListRedams
