8f1589dd583209b7ec20735a8ce6e4474a1bb340
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-06-07 13:56:33 -0500

    Inicia REDAM

---
+++ src/actions/RedamsActions.js
@@ -0,0 +1,40 @@
+import HttpCliente from '../services/HttpClientToken'
+
+
+export const Redams = (filtros) => {
+    return new Promise((resolve, reject) => {
+        const token = window.localStorage.getItem('token')
+        if (token) {
+            let ruta = '/v1/redam'
+            if (filtros.distrito_id) {
+                ruta = ruta + `?distrito_id=${filtros.distrito_id}`
+            }
+            if (filtros.domicilio_id) {
+                ruta = ruta + `?domicilio_id=${filtros.domicilio_id}`
+            }
+            HttpCliente.get(ruta, token)
+                .then(response => {
+                    resolve(response)
+                })
+                .catch(error => {
+                    resolve(error.response)
+                })
+        }
+    })
+}
+
+
+export const Redam = (id) => {
+    return new Promise((resolve, reject) => {
+        const token = window.localStorage.getItem('token')
+        if (token) {
+            HttpCliente.get(`/v1/redam/${id}`, token)
+                .then(response => {
+                    resolve(response)
+                })
+                .catch(error => {
+                    resolve(error.response)
+                })
+        }
+    })
+}
